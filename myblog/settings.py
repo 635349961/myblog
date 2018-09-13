1efc07c367e5c357056adde8f6b65d394e5041cb
sjj <1@1.com>
2018-09-13 18:18:24 +0800

    set debug=false for heroku

--- myblog/settings.py
+++ myblog/settings.py
@@ -149,7 +149,8 @@ if os.getcwd() == '/app':
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['blog-l.herokuapp.com']
+    DEBUG = False
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
